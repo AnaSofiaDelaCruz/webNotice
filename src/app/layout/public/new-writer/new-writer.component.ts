09f20ace635904050b258411378fba60badb958e
mauricioMatuz <killashe96@gmail.com>
2023-08-04 22:45:41 -0600

    Inicio de cero total

--- src/app/layout/public/new-writer/new-writer.component.ts
+++ src/app/layout/public/new-writer/new-writer.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { DashboardService } from 'src/service/DashboardService/dashboard-service.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-writer',
   templateUrl: './new-writer.component.html',
-  styleUrls: ['./new-writer.component.css']
+  styleUrls: ['./new-writer.component.css'],
 })
 export class NewWriterComponent {
-  public active : boolean = true;
+  public active: boolean = true;
   esAdmin = false;
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
@@ -18,16 +17,12 @@ export class NewWriterComponent {
       this.esAdmin = true;
     }
   }
-  constructor(
-    public router: Router,
-    private dashBoardService: DashboardService
-  ) {}
+  constructor(public router: Router) {}
   cerrarSesion(): void {
-    this.dashBoardService.logout();
     this.router.navigate(['/login']);
   }
 
-  setActive() : void {
+  setActive(): void {
     this.active = !this.active;
   }
 }
