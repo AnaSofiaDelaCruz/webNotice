8125847649b8d8da6fede6dcd681f404564e133c
Jaime Grimaldo <DankerGamer-@hotmail.com>
2023-08-31 09:28:35 -0600

    Agregando front login y registro movil

--- src/app/layout/public/noticia-chiapas/noticia-chiapas.component.ts
+++ src/app/layout/public/noticia-chiapas/noticia-chiapas.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-noticia-chiapas',
   templateUrl: './noticia-chiapas.component.html',
-  styleUrls: ['./noticia-chiapas.component.css']
+  styleUrls: ['../noticia-nacional/noticia-nacional.component.css'],
 })
 export class NoticiaChiapasComponent {
-
+  public active: boolean = true;
+  setActive(): void {
+    this.active = !this.active;
+  }
 }
--- src/app/layout/public/noticia-opinion/noticia-opinion.component.ts
+++ src/app/layout/public/noticia-opinion/noticia-opinion.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-noticia-opinion',
   templateUrl: './noticia-opinion.component.html',
-  styleUrls: ['./noticia-opinion.component.css']
+  styleUrls: [
+    '../noticia-nacional/noticia-nacional.component.css',
+  ],
 })
 export class NoticiaOpinionComponent {
-
+  public active: boolean = true;
+  setActive(): void {
+    this.active = !this.active;
+  }
 }
