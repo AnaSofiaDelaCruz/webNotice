266a874101787f697a72d0a76bad0aed43c06333
JaimeGrimaldo <DankerGamer-@hotmail.com>
2023-10-18 19:30:13 -0600

    Busqueda nueva completamente implementada

--- src/app/layout/public/resultados/resultados.component.ts
+++ src/app/layout/public/resultados/resultados.component.ts
@@ -1,18 +1,45 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { DashboardService } from 'src/app/service/DashboardService/dashboard.service';
 @Component({
   selector: 'app-resultados',
   templateUrl: './resultados.component.html',
-  styleUrls: ['./resultados.component.css']
+  styleUrls: ['./resultados.component.css'],
 })
 export class ResultadosComponent {
-  constructor(private route: ActivatedRoute) { }
+  public notasEncontradas: { items: any[]; itemPaths: string[] }[] = [];
+  public cantidad = 0;
+  constructor(
+    private route: ActivatedRoute,
+    private dashboardService: DashboardService,
+    private router: Router,
+  ) {}
+  busqueda = '';
   ngOnInit() {
-    this.route.params.subscribe(params => {
-      const parametro = params['parametro']; // Obtiene el valor del parámetro
+    this.route.params.subscribe((params) => {
+      const parametro = params['id']; // Obtiene el valor del parámetro
       // Utiliza el valor del parámetro en Componente B según tus necesidades
-      alert("TEnemos este dato:"+parametro)
+      this.busqueda = parametro;
     });
-    
+    this.Buscar()
+  }
+
+  public Buscar() {
+    this.dashboardService.BuscarNota(this.busqueda).subscribe((response) => {
+      // Almacenar las notas encontradas y cambiar el estado de busquedaRealizada
+      this.notasEncontradas = response.noticias.map((noticia) => ({
+        ...noticia,
+        itemPaths: noticia.items.map((item: { path: any }) => item.path),
+        item: noticia.items.map((item: { nombre: any }) => item.nombre),
+      })); // Asegúrate de que response contenga las notas buscadas
+      this.cantidad = this.notasEncontradas.length;
+    });
+  }
+
+  public LeerNota(id: string) {
+    this.router.navigate(['/news', id], {
+      queryParams: { parametro1: id },
+    });
   }
 }
